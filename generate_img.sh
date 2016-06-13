466cfedadac132beb42c7dade1d871290753bedb
Jens Maus <mail@jens-maus.de>
2016-06-13 20:30:25 +0200

    minor cleanup/update

--- generate_img.sh
+++ generate_img.sh
@@ -10,10 +10,11 @@ rm -rf tmp/*
 cp -a update_script tmp/
 cp -a watchdog tmp/
 cp -a rc.d tmp/
+cp -a www tmp/
 cp -a VERSION tmp/www/
 
 # generate archive
 cd tmp
-tar --owner=root --group=root --exclude=.DS_Store -czvf ../hm_pdetect-$(cat ../VERSION).tar.gz *
+tar --owner=root --group=root --exclude=.DS_Store -czvf ../hm-watchdog-$(cat ../VERSION).tar.gz *
 cd ..
 rm -rf tmp
